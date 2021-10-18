37fb2793af128dbfb8eb6b1fe5dbfe13a52e3bb5
Đinh Quang Anh <91303593+DQuangAnh@users.noreply.github.com>
2021-10-18 12:01:57 +0700

    Add files via upload

---
+++ sx mang giam dan.c
@@ -0,0 +1,38 @@
+// sap xep mang giam dan
+
+#include<stdio.h>
+
+main()
+{
+	int num[10];
+	int i, j, gantam;
+	int n;
+	
+	printf("\nNhap vao so luong phan tu cua day so: ");
+	scanf("%d", &n);
+	
+	for(i = 0; i < n; i++){
+		
+		printf("\nNhap 1 so bat ki: ");
+		scanf("%d", &num[i]);
+		}
+	for(i = 0; i < n - 1; i++)
+	 {   
+		for(j = i + 1; j < n; j++)
+		 {
+		    if(num[i] < num[j])
+			  {
+			      gantam = num[i];
+				  num[i] = num[j];
+				  num[j] = gantam; 	
+		      }
+         }
+     }
+	 
+	printf("\nDay so sap xep giam dan la : ");
+	for(i = 0; i < n; i++)
+	{
+	    printf("%d ", num[i]);
+	}	     
+	return 0;
+	}
